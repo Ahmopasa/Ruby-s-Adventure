6ed1989b235a0ec4c5af9dba1382a6bced617d3a
Ahmet Kandemir PEHLİVANLI <ahmet.pehlivanli@outlook.com>
2022-02-26 04:20:34 +0300

    - In this update, * We can move the Ruby on horizontal axis and on vertical axis.
    * We observed the effects of using 'Time.deltaTime'.

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -1,3 +1,5 @@
+//# The effects of Time.deltaTime #
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,16 +9,21 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10; // Makes the FPS 10.
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        Debug.Log("Horizontal: " + horizontal);
+
+        float vertical = Input.GetAxis("Vertical");
+
         Vector2 position = transform.position;
-
-        position.x = position.x + 0.1f;
-
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
