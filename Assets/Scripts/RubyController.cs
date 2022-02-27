ec35e4c7b9d85b420eb29a33606f246a8f92758d
Ahmet Kandemir PEHLİVANLI <ahmet.pehlivanli@outlook.com>
2022-02-27 03:43:19 +0300

    - In this update, * Tilesets were being added, * Game scene were decorated.

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -9,8 +9,8 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 10; // Makes the FPS 10.
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10; // Makes the FPS 10.
     }
 
     // Update is called once per frame
